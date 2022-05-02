d8bc3acb9b3dcbe1cd128836e2c05c832cdf60a3
devthalal <thalal.hassan@neoito.com>
2022-05-02 17:58:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const hireGetCandidate = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello hireGetCandidate`}))
+  res.end()
+  
+}
